5731c0ce6a030b57004b4fb313858a43db7f04bd
Auro Tripathy <ipserv@yahoo.com>
2018-11-15 16:29:58 -0800

    after refacto

--- samples/shapes/train_shapes.py
+++ samples/shapes/train_shapes.py
@@ -18,7 +18,7 @@ from mrcnn.config import Config
 from mrcnn import utils
 import mrcnn.model as modellib
 from mrcnn import visualize
-from mrcnn.model import log
+from mrcnn.utils import log
 
 # %matplotlib inline
 
