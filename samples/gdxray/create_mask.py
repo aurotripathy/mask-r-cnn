80b3ee012b19c9f7d59b45d7cdb8ce54d1f0cd6c
Auro Tripathy <ipserv@yahoo.com>
2019-03-13 22:28:58 -0700

    got file name

---
+++ samples/gdxray/create_mask.py
@@ -0,0 +1,39 @@
+import json
+import glob
+import os
+from pudb import set_trace
+
+ROOT_DIR = '/home/auro/via/nirmalyalabs-work/'
+
+
+# https://gist.github.com/douglasmiranda/5127251
+def find(key, dictionary):
+    for k, v in dictionary.items():
+        if k == key:
+            yield v
+        elif isinstance(v, dict):
+            for result in find(key, v):
+                yield result
+        elif isinstance(v, list):
+            for d in v:
+                for result in find(key, d):
+                    yield result
+
+
+for name in glob.glob(ROOT_DIR + '/*/*/*.json'):
+    print('\nFile name:', name)
+    image_name_list = glob.glob(os.path.dirname(name) + '/*.png')
+    if len(image_name_list) != 1 and not os.path.isfile(image_name_list[0]):
+        print('File {}, does not exist'.format(image_name_list))
+    print('\nImage file name:', image_name_list[0])
+    with open(name, "r") as read_file:
+        regions = json.load(read_file)
+
+    region_list = list(find('regions', regions))
+    print('\nRegion list')
+    print(region_list)
+
+    print('\nEnumerate shapes')
+    for shape_attrib in region_list[0]:
+        print('all_points_x', shape_attrib['shape_attributes']['all_points_x'])
+        print('all_points_y', shape_attrib['shape_attributes']['all_points_y'])
